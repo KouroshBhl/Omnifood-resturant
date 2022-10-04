23c8701bcaab5f0827b6691e08f0f22fe00219af
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-10-04 22:58:37 +0330

    Implement learn more button scroll into section1

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const navBar = document.querySelector(".ul-nav");
-const learnMore = document.querySelector(".btn--info");
+const navBar = document.querySelector('.header--nav');
+const learnMore = document.querySelector('.btn--info');
+const section1 = document.getElementById('section1');
+
+learnMore.addEventListener('click', function (e) {
+  e.preventDefault();
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
